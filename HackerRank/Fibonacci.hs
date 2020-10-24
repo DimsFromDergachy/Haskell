24ee9ecb2aca42292fa3eff73d0c3ed455a03481
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2020-10-24 22:53:35 +0400

    Solve Fibonacci problem on HackerRank

---
+++ HackerRank/Fibonacci.hs
@@ -0,0 +1,18 @@
+-- https://www.hackerrank.com/challenges/fibonacci-fp/problem
+
+--  [0, 1, 1, 2, 3, 5 ...]
+--   +
+--  [1, 1, 2, 3, 5, 8 ...]
+-- -------------------------
+--  [1, 2, 3, 5, 8, 13 ...]
+
+import Control.Monad (replicateM_)
+
+fibonacci :: [Int]
+fibonacci  =  0 : 1 : zipWith (+%) fibonacci (tail fibonacci)
+  where
+    (+%) :: Int -> Int -> Int
+    (+%) a b  =  (a + b) `mod` (10^8 + 7)
+
+main :: IO ()
+main  =  readLn >>= flip replicateM_ (readLn >>= print . (fibonacci !!))
