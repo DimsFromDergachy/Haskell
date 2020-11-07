5de8ddb6ad5c28f81292ccbdab0501792e16665d
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2020-11-07 20:36:20 +0400

    Simplify test

--- HackerRank/ConcavePolygon/test/ConcavePolygonTests.hs
+++ HackerRank/ConcavePolygon/test/ConcavePolygonTests.hs
@@ -10,24 +10,18 @@ assertIsNotConcave s  =  assertBool s
 tests :: Test
 tests  =  TestList
   [
-    TestCase $ assertIsConcave "Triangle 1" $ tester [0,0, 0,1, 1,0],
-    TestCase $ assertIsConcave "Sample Input" $ tester [0,0, 0,1, 1,1, 1,0],
-    TestCase $ assertIsConcave "Sample Input #0" $ tester [622,991, 1054,665, 661,485],
-    TestCase $ assertIsConcave "Sample Input #1" $ tester
+    testCase "Triangle" assertIsConcave [0,0, 0,1, 1,0],
+    testCase "Sample Input" assertIsConcave [0,0, 0,1, 1,1, 1,0],
+    testCase "Sample Input #0" assertIsConcave [622,991, 1054,665, 661,485],
+    testCase "Sample Input #1" assertIsConcave
       [1042,943, 793,1042, 404,909, 574,474, 1077,721, 392,543, 572,1005, 963,1020, 857,390]
   ]
 
-testCase :: String -> Assertion -> [Int] -> Test
-testCase ps = undefined
+testCase :: String -> (String -> Bool -> Assertion) -> [Int] -> Test
+testCase message assert  =  TestCase . (assert message) . solve . toPoints'
   where
     toPoints' [] = []
     toPoints' (x:y:xs) = (x,y):toPoints' xs
-
-tester :: [Int] -> Bool
-tester  =  solve . tester'
-  where
-    tester' []        =  []
-    tester' (x:y:xs)  =  (x,y) : tester' xs
 
 main :: IO ()
 main = do
