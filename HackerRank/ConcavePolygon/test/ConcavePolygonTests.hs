da0131b87501443ec72ed4efe27e9e11d979422f
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2020-11-07 20:49:32 +0400

    Add tests

--- HackerRank/ConcavePolygon/test/ConcavePolygonTests.hs
+++ HackerRank/ConcavePolygon/test/ConcavePolygonTests.hs
@@ -14,7 +14,10 @@ tests  =  TestList
     testCase "Sample Input" assertIsConcave [0,0, 0,1, 1,1, 1,0],
     testCase "Sample Input #0" assertIsConcave [622,991, 1054,665, 661,485],
     testCase "Sample Input #1" assertIsConcave
-      [1042,943, 793,1042, 404,909, 574,474, 1077,721, 392,543, 572,1005, 963,1020, 857,390]
+      [1042,943, 793,1042, 404,909, 574,474, 1077,721, 392,543, 572,1005, 963,1020, 857,390],
+    testCase "No concave" assertIsNotConcave [0,0, 3,0, 1,1, 0,3],
+    testCase "Zero cross" assertIsConcave [0,0, 2,0, 2,2, 0,2, 0,1],
+    testCase "Mix vertex" assertIsConcave [0,0, 1,1, 0,1, 1,0]
   ]
 
 testCase :: String -> (String -> Bool -> Assertion) -> [Int] -> Test
