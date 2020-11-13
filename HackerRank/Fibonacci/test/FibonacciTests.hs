12b59cbccbbb9732618fb4ca67404634125c13ed
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2020-11-13 23:33:12 +0400

    Add UnitTests for 'Fibonacci' project

---
+++ HackerRank/Fibonacci/test/FibonacciTests.hs
@@ -0,0 +1,35 @@
+module Main (main) where
+
+import Fibonacci (fibonacci)
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.Options
+
+simpleTests :: TestTree
+simpleTests  =  testGroup "Fibonacci's tests"
+  [
+    test "Zeroth" 0 (fibonacci !! 0),
+    test "First"  1 (fibonacci !! 1),
+    test "Second" 1 (fibonacci !! 2),
+    test "Third"  2 (fibonacci !! 3),
+    test "Max by module" (102334155 `mod` (10^8 + 7)) (fibonacci !! 40)
+  ]
+  where
+    test :: String -> Int -> Int -> TestTree
+    test name exp  =  testCase name . assertEqual "" exp
+
+allNumbersTests :: TestTree
+allNumbersTests  =  testGroup "Fibonacci's test all numbers"
+  $ zipWith test [0..] $
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55] ++
+    [89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765] ++
+    [10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040] ++
+    [1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986]
+  where
+    test n exp  =  testCase (show n) $ assertEqual "" exp (fibonacci !! n)
+
+unitTests :: TestTree
+unitTests  =  testGroup "Unit tests" [simpleTests, allNumbersTests]
+
+main :: IO ()
+main  =  defaultMain unitTests
