0a5a4e3f46ecef867192cbb6e2284b3a1303335a
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2020-12-09 23:44:57 +0400

    Use ParserCombinators (Advent Of Code 2020 - Day 2)

--- AdventOfCode/2020/Day2.hs
+++ AdventOfCode/2020/Day2.hs
@@ -1,27 +1,46 @@
 -- Day 2: Password Philosophy
 -- https://adventofcode.com/2020/day/2
 
+import Data.Char (isDigit)
+import Text.ParserCombinators.ReadP
+
+type Policy = (Int, Int, Char, String)
+
+getNumber :: ReadP Int
+getNumber = pure read <*> munch1 isDigit
+
+getRest :: ReadP String
+getRest = manyTill get eof
+
 -- 1-3 a: abcde
 -- 1-3 b: cdefg
 -- 2-9 c: ccccccccc
--- TODO: Use a parser (e.g. readP)
-parse :: String -> (Int, Int, Char, String)
-parse s = (read a, read b, head cs, ps)
-  where
-    [a, b, cs, ps] = words $ map (\c -> if c == '-' then ' ' else c) s
+policyParser :: ReadP Policy
+policyParser = pure (,,,)
+    <*> getNumber
+    <*  char '-'
+    <*> getNumber
+    <*  skipSpaces
+    <*> get
+    <*  char ':'
+    <*  skipSpaces
+    <*> getRest
 
-matchPolicyI :: (Int, Int, Char, String) -> Bool
+parse :: String -> Policy
+parse = fst . head . readP_to_S policyParser
+
+matchPolicyI :: Policy -> Bool
 matchPolicyI (a, b, c, p) = a <= m && m <= b
   where
     m = length $ filter (== c) p
 
-matchPolicyII :: (Int, Int, Char, String) -> Bool
+matchPolicyII :: Policy -> Bool
 matchPolicyII (a, b, c, p) = (p !! (a-1) == c) `xor` (p !! (b-1) == c)
   where
-    (xor) a b = a /= b    
+    (xor) a b = a /= b
 
 solve :: [String] -> Int
-solve = length . filter matchPolicyII . map parse
+solve = length . filter (matchPolicyII . parse)
 
 main :: IO ()
 main = readFile "day2.input" >>= pure . lines >>= print . solve
