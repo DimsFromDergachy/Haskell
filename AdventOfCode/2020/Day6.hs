b256b942ddd3b916bcdb98f5f9d15aac349d2a59
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-01-06 21:00:15 +0400

    Solve 'Day 6B: Custom Customs'

--- AdventOfCode/2020/Day6.hs
+++ AdventOfCode/2020/Day6.hs
@@ -5,11 +5,17 @@ import Data.Char (isSpace)
 import Data.List (nub)
 import Data.List.Split (splitOn)
 
-parseGroup :: String -> Int
-parseGroup = length . nub . filter (not . isSpace)
+solveA :: String -> Int
+solveA = length . nub . filter (not . isSpace)
 
 partA :: String -> Int 
-partA = sum . map parseGroup . splitOn "\n\n"
+partA = sum . map solveA . splitOn "\n\n"
+
+solveB :: [String] -> Int
+solveB ss = length [() | c <- ['a' .. 'z'], all (c `elem`) ss]
+
+partB :: String -> Int
+partB = sum . map (solveB . lines) . splitOn "\n\n"
 
 main :: IO()
-main = getContents >>= print . partA
+main = getContents >>= print . partB
