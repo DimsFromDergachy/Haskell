0f3ae093c6adaad3c3b3afb60cd6425f01a02978
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-01-06 16:47:47 +0400

    Solve 'Day 5B: Binary Boarding'

--- AdventOfCode/2020/Day5.hs
+++ AdventOfCode/2020/Day5.hs
@@ -2,7 +2,8 @@
 -- https://adventofcode.com/2020/day/5
 
 import Data.Bifunctor (bimap)
-import Data.Bits
+import Data.Bits ((.|.), shift, zeroBits)
+import Data.List (sort)
 
 fromBits :: (Char -> Bool) -> String -> Int
 fromBits isBit = foldl (\r c -> shift r 1 .|. fromEnum (isBit c)) zeroBits
@@ -11,8 +12,19 @@ fromBits isBit = foldl (\r c -> shift r 1 .|. fromEnum (isBit c)) zeroBits
 parseSeat :: String -> (Int, Int)
 parseSeat = bimap (fromBits (== 'B')) (fromBits (== 'R')) . splitAt 7
 
+parseId :: String -> Int
+parseId = (\(a, b) -> a*8 + b) . parseSeat
+
 partA :: [String] -> Int
 partA = maximum . map ((\(a, b) -> a*8 + b) . parseSeat)
 
+findHole :: [Int] -> Int
+findHole = (+1) . fst . head . dropWhile (\(a, b) -> a + 1 == b) . pair . sort
+  where
+    pair xs = zip xs $ tail xs
+
+partB :: [String] -> Int
+partB = findHole . map parseId
+
 main :: IO()
-main = getContents >>= print . partA . lines
+main = getContents >>= print . partB . lines
