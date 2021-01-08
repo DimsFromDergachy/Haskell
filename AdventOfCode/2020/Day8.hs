be93329455f84239e2382979f96cd9aae8550720
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-01-09 01:40:11 +0400

    Fix solution 'Day 8A: Handheld Halting'

--- AdventOfCode/2020/Day8.hs
+++ AdventOfCode/2020/Day8.hs
@@ -2,18 +2,37 @@
 -- https://adventofcode.com/2020/day/8
 
 import Data.Array ( (!), listArray )
+import Data.Char ( isSpace )
 import Data.Set ( empty, insert, member )
+import Text.ParserCombinators.ReadP
 
 data Op = Nop | Acc Int | Jmp Int
   deriving Eq
 
+nop :: ReadP Op
+nop = Nop <$ string "nop"
+
+acc :: ReadP Op
+acc = do
+    string "acc"
+    skipSpaces
+    optional $ char '+'
+    arg <- munch1 $ not . isSpace
+    pure $ Acc $ read arg
+
+jmp :: ReadP Op
+jmp = do
+    string "jmp"
+    skipSpaces
+    optional $ char '+'
+    arg <- munch1 $ not . isSpace
+    pure $ Jmp $ read arg
+
+parser :: ReadP Op
+parser = choice [nop, acc, jmp]
+
 parse :: String -> Op
-parse line
-  | op == "nop" = Nop
-  | op == "acc" = Acc $ read arg
-  | op == "jmp" = Jmp $ read arg
-  where
-    [op, arg] = words line
+parse = fst . head . readP_to_S parser
 
 partA :: [String] -> Int 
 partA lines = execute 0 1 empty
