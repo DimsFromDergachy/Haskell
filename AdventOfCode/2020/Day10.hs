0533691d52d5bc8579f7043c4560a7c45dd9d8c6
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-01-17 23:58:06 +0400

    Solve 'Day 10B: Adapter Array'

--- AdventOfCode/2020/Day10.hs
+++ AdventOfCode/2020/Day10.hs
@@ -1,18 +1,28 @@
 -- Day 10: Adapter Array
 -- https://adventofcode.com/2020/day/10
 
+import Data.Array ((!), accumArray)
 import Data.List (sort)
 
 parseInput :: String -> [Int]
 parseInput = map read . lines
 
 zipNeighbors :: (a -> a -> b) -> [a] -> [b]
-zipNeighbors f xs  =  zipWith f xs (tail $ cycle xs)
+zipNeighbors f xs  =  zipWith f xs $ tail $ cycle xs
 
 partA :: [Int] -> Int
 partA xs = (1 + length (filter (== 1) ds)) * (1 + length (filter (== 3) ds))
   where
     ds = zipNeighbors (flip (-)) $ sort xs
 
+-- d[x] - amount of ways to reach x-th adapter (0 if adapter doesn't exist)
+-- d[x] = d[x-1] + d[x-2] + d[x-3]
+partB :: [Int] -> Int
+partB xs = d ! n
+  where
+    d = accumArray (flip const) 0 (-2, n) $
+      (0, 1) : [(x, d ! (x-3) + d ! (x-2) + d ! (x-1)) | x <- xs]
+    n = maximum xs
+
 main :: IO ()
-main = getContents >>= print . partA . parseInput
+main = getContents >>= print . partB . parseInput
