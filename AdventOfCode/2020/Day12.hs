c7a000dc67105bfa1c90a5cde5e06f84999e0828
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-02-15 00:37:40 +0400

    Correct left & rigth turning (Day 12A)

--- AdventOfCode/2020/Day12.hs
+++ AdventOfCode/2020/Day12.hs
@@ -8,6 +8,10 @@ type Ship = (Direction, Position)
 start :: Ship
 start = ((1, 0), (0, 0))
 
+left, right :: Direction -> Direction
+left  (dx, dy) = (-dy, dx)
+right (dx, dy) = (dy, -dx)
+
 move :: Ship -> String -> Ship
 move ((dx, dy), (x, y)) line = case c of
     'E' -> ((dx, dy), (x + z, y))
@@ -15,10 +19,11 @@ move ((dx, dy), (x, y)) line = case c of
     'W' -> ((dx, dy), (x - z, y))
     'N' -> ((dx, dy), (x, y + z))
     'F' -> ((dx, dy), (x + dx * z, y + dy * z))
-    'L' -> ((-dy, dx), (x, y))
-    'R' -> ((dy, -dx), (x, y))
+    'L' -> (turn left (dx, dy), (x, y))
+    'R' -> (turn right (dx, dy), (x, y))
   where
     (c, z) = (head line, read $ tail line)
+    turn f = foldl (.) id (replicate (z `div` 90) f)
 
 distance :: Ship -> Int
 distance (_, (x, y)) = abs x + abs y
